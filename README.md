6b68b6cc3cc6e5d294cdc90bb2a684cc5f1a0a99
James Swineson <github@public.swineson.me>
2019-03-02 23:19:41 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # vps2routeros
-Install RouterOS without screen and keyboard, only Telnet/SSH is required.
+
+This script will wipe your current OS then install RouterOS on the target computer without requiring physical access. It has been proven to work with a wide range of VPSs.
+
+This project is previously hosted on a [Gist](https://gist.github.com/Jamesits/6f208ce814e815c1da39726c873de152), but the script become larger over time and a gist can not fit it anymore, so it is moved here.
+
+## WARNING
+
+> Your warranty is now void. I am not responsible for bricked devices, dead SD cards, thermonuclear war, or you getting fired because the alarm app failed. Please do some research if you have any concerns about features included in this ROM before flashing it! YOU are choosing to make these modifications, and if you point the finger at me for messing up your device, I will laugh at you.
+
+* **PLEASE READ THIS DOCUMENTATION THROUGHLY BEFORE RUNNING**
+* CAUTION: ALL DATA ON YOUR DEVICE WILL BE LOST INSTANTLY! There is no way to get them back.
+* Please make a backup of your existing network configuration prior to running this in case of any problem
+* If you use IPv6, remember your gateway link-local address too
+* Please SET AN PASSWORD IMMEDIATELY after installation
+* Please UPDATE IMMEDIATELY after installation
+
+## Usage
+
+### Requirements
+
+The target device:
+
+* is amd64 (sometimes called x86_64) architecture
+* is a physical device or a full-virtualized VM
+* runs Ubuntu 16.04 or higher (we recommend Ubuntu 16.04; other distros are not tested)
+
+### Running the script
+
+1\. download this script to the target computer:
+
+```shell
+# use wget
+wget https://go.swineson.me/vps2routeros
+
+# or use curl
+curl -L https://go.swineson.me/vps2routeros
+```
+
+2\. Use any text editor to open the script, change the default config if needed
+
+3\. run it:
+
+```shell
+chmod +x ./vps2routeros
+sudo ./vps2routeros
+```
+
+4\. Set a password:
+
+Login with WinBox or HTTP or SSH using username `admin` and an empty password, then change the password.
+
+## Documentation
+
+* [A detailed explanation of this script written by me](https://blog.swineson.me/install-routeros-on-any-ubuntu-vps/) (in Chinese)
+* This script is originated from [this post on DigitalOcean forum](https://www.digitalocean.com/community/questions/installing-mikrotik-routeros)
---
+++ vps2routeros.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+# VPS2RouterOS
+# https://github.com/Jamesits/vps2routeros
+# This script will cause permanent data loss
+# Please read the documentation prior to running this
+# You have been warned
+
+# ======================= please change these =================================
+# your network interface to internet
+# this is used to auto configure IPv4 after reboot
+# (this may not work for every device)
+# eth0 for most devices, ens3 for Vultr
+# you can use `ip addr` or `ifconfig` to find out this
+# default: the interface on the default route
+MAIN_INTERFACE=$(ip route list | grep default | cut -d' ' -f 5)
+
+# HDD device (not partition)
+# May not be compatible with SCSI drives; see official document of RouterOS CHR
+# you can use `lsblk` to find out this
+# default: the disk with a partition mounted to `/`
+DISK=$(mount | grep ' / ' | cut -d' ' -f1 | sed 's/[0-9]*$//g')
+
+# get IPv4 address in IP-CIDR format
+# do not modify unless you know what you are doing
+ADDRESS=$(ip addr show $MAIN_INTERFACE | grep global | cut -d' ' -f 6 | head -n 1)
+
+# get gateway IP
+# do not modify unless you know what you are doing
+GATEWAY=$(ip route list | grep default | cut -d' ' -f 3)
+
+# URL to RouterOS CHR
+ROUTEROS_URL=https://download2.mikrotik.com/routeros/6.42.12/chr-6.42.12.img.zip
+
+# Note: you can customize commands to be executed when RouterOS initializes.
+# Search `Auto configure script` below
+# do not modify that unless you know what you are doing
+
+# ======================= no need to modify below ============================
+
+set -euo pipefail
+
+# check if this script is running under root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# https://stackoverflow.com/a/3232082/2646069
+confirm() {
+    # call with a prompt string or use a default
+    read -r -p "${1:-Are you sure? [y/N]} " response
+    case "$response" in
+        [yY][eE][sS]|[yY]) 
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+echo -e "Please confirm the settings:"
+echo -e "Installation destination: ${DISK}"
+echo -e "Network information:"
+echo -e "\tinterface: ${MAIN_INTERFACE}"
+echo -e "\tIPv4 address: ${ADDRESS}"
+echo -e "\tIPv4 gateway: ${GATEWAY}"
+echo -e "\nIf you continue, your disk will be formatted and no data will be preserved."
+
+confirm || exit -1
+
+echo "installing packages"
+apt update -y
+apt install -y qemu-utils pv psmisc
+
+echo "download image"
+wget ${ROUTEROS_URL} -O chr.img.zip
+
+echo "unzip image"
+gunzip -c chr.img.zip > chr.img
+
+echo "convert image"
+qemu-img convert chr.img -O qcow2 chr.qcow2
+qemu-img resize chr.qcow2 `fdisk $DISK -l | head -n 1 | cut -d',' -f 2 | cut -d' ' -f 2`
+
+echo "mount image"
+modprobe nbd
+qemu-nbd -c /dev/nbd0 chr.qcow2
+echo "waiting qemu-nbd"
+sleep 5
+partprobe /dev/nbd0
+mount /dev/nbd0p2 /mnt
+
+echo "write init script"
+cat > /mnt/rw/autorun.scr <<EOF
+# Auto configure script on RouterOS first boot
+# feel free to customize it if you really need
+/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]
+/ip route add gateway=$GATEWAY
+/ip service disable telnet
+/ip dns set servers=8.8.8.8,8.8.4.4
+EOF
+
+echo "unmount image"
+umount /mnt
+
+echo "resize partition"
+echo -e 'd\n2\nn\np\n2\n65537\n\nw\n' | fdisk /dev/nbd0
+e2fsck -f -y /dev/nbd0p2 || true
+resize2fs /dev/nbd0p2
+sleep 5
+
+echo "move image to RAM (this will take quite a while)"
+mount -t tmpfs tmpfs /mnt
+pv /dev/nbd0 | gzip > /mnt/chr-extended.gz
+sleep 5
+
+echo "stop qemu-nbd"
+killall qemu-nbd
+sleep 5
+echo u > /proc/sysrq-trigger
+sleep 5
+
+echo "Your old OS is being wiped while running, good luck"
+echo "If the device stopped responding for more than 30 minutes, please issus a reboot manually"
+
+echo "write disk"
+zcat /mnt/chr-extended.gz | pv > $DISK
+
+echo "sync disk"
+echo s > /proc/sysrq-trigger
+
+echo "wait a while"
+sleep 5 || echo "please wait 5 seconds and execute\n\techo b > /proc/sysrq-trigger\nmanually, or hard reset device"
+
+echo "rebooting"
+echo b > /proc/sysrq-trigger
